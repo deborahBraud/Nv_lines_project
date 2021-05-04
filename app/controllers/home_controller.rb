b0b7e250fe3b2b94c0a7d318e7365182fd5f6178
Boris MEYER <bmeyer@unice.fr>
2021-05-04 18:05:17 +0200

    Sidebar RAILS

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,8 @@
 class HomeController < ApplicationController
   def home_page
+  	@phenotypes = Phenotype.all
+  	@sublocalization_type = sublocalization_type
+  	@region_type = region_type
+  	@cell_type = cell_type
   end
 end
