9701ebd168c72f37abd353d79758450e92fd4a37
Boris MEYER <contact@webenly.fr>
2021-05-25 15:59:26 +0200

    Publication : Production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -119,4 +119,7 @@ Rails.application.configure do
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
   #config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  # Website URL
+    config.hosts << "nvlines.ircan.org
 end
